77671b87c9830fccae6efe6bd4a135d377a7b535
root <root@localhost.localdomain>
2015-04-08 15:45:27 +0200

    small changes in example

--- smb_enumerator.py
+++ smb_enumerator.py
@@ -1,18 +1,19 @@
-#!/usr/bin/python
-
 import sys
 from smb.SMBConnection import SMBConnection
 
+print ("Usage:   python smb_enumerator 'username' 'password' 'system' 'domain'")
+
 conn = SMBConnection(sys.argv[1],
     sys.argv[2],
-    'MyApp',
-    '192.168.5.128',
-    '',
+    'enumerator',
+    sys.argv[3],
+    sys.argv[4],
     use_ntlm_v2=True,
     sign_options=SMBConnection.SIGN_WHEN_SUPPORTED,
     is_direct_tcp=True)
-connected = conn.connect('192.168.5.128',445)
+
+connected = conn.connect(sys.argv[3],445)
 Response = conn.listShares(timeout=30)
 
 for i in range(len(Response)):
-        print("Share[",i,"] =", Response[i].name)
+    print("Share[",i,"] =", Response[i].name)
