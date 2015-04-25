73a14243e74abf796c0da91c741024cbcbf618b8
n3if <oriolnf@gmail.com>
2015-04-25 18:41:46 +0200

    load targets from file

--- smb_enumerator.py
+++ smb_enumerator.py
@@ -1,41 +1,43 @@
 import sys
 from smb.SMBConnection import SMBConnection
 
-print ("### Usage:  python smb_enumerator 'username' 'password' 'system' 'domain'")
+print ("### Usage:  python smb_enumerator 'username' 'password' 'ip/systems file' 'domain'")
 
+with open(sys.argv[3]) as f:
+    for line in f:
+        try:
+            print('### Analyzing system: ' + line)
+            # parameterize an smb connection with a system
+            conn = SMBConnection(sys.argv[1],
+                sys.argv[2],
+                'enumerator',
+                line,
+                sys.argv[4],
+                use_ntlm_v2=True,
+                sign_options=SMBConnection.SIGN_WHEN_SUPPORTED,
+                is_direct_tcp=True)
 
-try:
-# parameterize an smb connection with a system
-    conn = SMBConnection(sys.argv[1],
-        sys.argv[2],
-        'enumerator',
-        sys.argv[3],
-        sys.argv[4],
-        use_ntlm_v2=True,
-        sign_options=SMBConnection.SIGN_WHEN_SUPPORTED,
-        is_direct_tcp=True)
+            # establish the actual connection
+            connected = conn.connect(sys.argv[3],445)
 
-        # establish the actual connection
-    connected = conn.connect(sys.argv[3],445)
-
-    try:
-        Response = conn.listShares(timeout=30)  # obtain a list of shares
-        print('Shares on: ' + sys.argv[3])
-        for i in range(len(Response)):  # iterate through the list of shares
-            print("  Share[",i,"] =", Response[i].name)
-                    
             try:
-                # list the files on each share (recursivity?)
-                Response2 = conn.listPath(Response[i].name,'/',timeout=30)
-                print('    Files on: ' + sys.argv[3] + '/' + "  Share[",i,"] =",
-                       Response[i].name)
-                for i in range(len(Response2)):
-                    print("    File[",i,"] =", Response2[i].filename)
+                Response = conn.listShares(timeout=30)  # obtain a list of shares
+                print('Shares on: ' + sys.argv[3])
+                for i in range(len(Response)):  # iterate through the list of shares
+                    print("  Share[",i,"] =", Response[i].name)
+                            
+                    try:
+                        # list the files on each share (recursivity?)
+                        Response2 = conn.listPath(Response[i].name,'/',timeout=30)
+                        print('    Files on: ' + sys.argv[3] + '/' + "  Share[",i,"] =",
+                               Response[i].name)
+                        for i in range(len(Response2)):
+                            print("    File[",i,"] =", Response2[i].filename)
+                    except:
+                        print('### can not access the resource')
+
             except:
-                print('### can not access the resource')
+                print('### can not list shares')
 
-    except:
-        print('### can not list shares')
-
-except:
-    print('### can not access the system')
+        except:
+            print('### can not access the system')
